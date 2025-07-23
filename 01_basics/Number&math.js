9bf6aa994b09a92de0e618c86f9eb860b663718f
Ankit Singh <as3542429@gmail.com>
2025-07-23 13:01:35 +0000

    upto dates almost completed

---
+++ 01_basics/Number&math.js
@@ -0,0 +1,38 @@
+// const score = 100;
+// console.log(score);
+
+
+let balance = new Number(250.3669);
+// console.log(balance);
+// console.log(typeof (balance.toFixed(5).toString()));
+// console.log(balance.toFixed(5));
+// console.log(balance.toString().length);
+// console.log(balance.toPrecision(4)); // starting digit at index 1
+// console.log(balance.toExponential(1)); //2.5e+2 => 2.5*10pow(2); 250
+
+// +++++++++++++++++++++++++++++ math +++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math); to know all functionalities and properties of math just console it to the browser
+// console.log(Math.PI);
+// const pi = Math.PI;
+// console.log(pi.toFixed(3));
+// console.log(Math.SQRT2);
+// console.log(Math.abs(-50));
+
+
+// round off 
+// console.log(Math.round(5.923));
+// console.log(Math.floor(9.9))
+// console.log(Math.ceil(9.0000001));
+
+// random
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10 + 1);
+// console.log(Math.floor(Math.random()*1000 +1)); //to remove decimal
+let min = 1;
+let max = 6;
+console.log(Math.floor(Math.random()*(max - min +1) + min));
+
+
+
+
---
+++ 01_basics/dates.js
@@ -0,0 +1,15 @@
+let myDate = new Date("August,18,2002");
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString("en-US", { timeZone: "UTC" }));
+// console.log(myDate.getMonth());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+console.log(myDate.getDay());
+console.log(myDate.getTime());
+
+// let newMonth = myDate.setMonth(8);
