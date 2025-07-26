b55835f1c703d239b9fecb1753afe75b0273a1ed
Ankit Singh <as3542429@gmail.com>
2025-07-26 13:21:41 +0000

    arrays completed

--- 01_basics/dates2.js
+++ 01_basics/dates2.js
@@ -1,26 +1,46 @@
-// DATES
+// dates part 2 
 
-// creations 
-let date1 = new Date();
-// console.log(date1.toLocaleDateString()); // it returns local way of dates like in india dd/mm/yy
+// creations
+const date1 = new Date(); //current date
+// console.log(date1.toLocaleString());
 
-let date2 = new Date(2025,0,1);
+const date2 = new Date(2025,0,1); // yy/mm/dd
 // console.log(date2.toString());
-// console.log(date2.toDateString()); // it returns (day mm dd yy)
 
-let date3 = new Date(2025,0,2,5,3,5);
-// console.log(date3.toString()); //it returns a string with some detail of dates and time combination
+const date3 = new Date(1,0,2025); //dd/mm/yy
+// console.log(date2.toString());
 
-let date4 = new Date("2025-01-25");
-// console.log(date4.toLocaleString());
+const date4 = new Date(2025,0,1,17,30); //dd/mm/yy
+// console.log(date4.toString());
 
 
-// conversion of time
-const today = Date.now();
-console.log(today); //miliseconds
-console.log(Math.floor(today/1000)); // seconds
-console.log(Math.floor((today/1000)/60)); // minutes
-console.log(Math.floor(( (today/1000) /60)/60)); //hours
-console.log(Math.floor((( (today/1000) /60)/60)/24)); //days
-console.log(Math.floor(((( (today/1000) /60)/60)/24)/365)); //years
-console.log(Math.ceil(((( (today/1000) /60)/60)/24)%365.25)); //remaining days
+const date5 = new Date("2025/7/20"); // when we pass a date as string then our month indexing start from 1;
+// console.log(date5);
+
+// ++++++++++++++++++++++ explore +++++++++++++++++++++
+
+const currentDate = Date.now();
+// console.log("milliseconds",currentDate); //milliseconds 
+// it return always milliseconds from 1970/1/1
+// console.log("seconds",Math.floor(currentDate/1000));
+// console.log("minutes",Math.floor((currentDate/1000)/60));
+// console.log("hours",Math.floor(((currentDate/1000)/60)/60));
+// console.log("days",Math.floor((((currentDate/1000)/60)/60)/24));
+// console.log("years",Math.floor(((((currentDate/1000)/60)/60)/24)/365.25));
+// console.log("years2",Math.floor(currentDate/(1000*60*60*24*365.25)));
+// console.log("+ days",Math.ceil(((((currentDate/1000)/60)/60)/24)%365.25));
+
+let babluDob = new Date(2006,9,3);
+let current = new Date();
+let gapDobInMs = current - babluDob; //millisecons 
+
+let ageDate = new Date(gapDobInMs); // it calculates date from 1970 of difference 
+let years = ageDate.getUTCFullYear()-1970;
+let years2 = ageDate.getUTCFullYear();
+let month = ageDate.getUTCMonth();
+let day = ageDate.getUTCDate();
+
+console.log(`Gap from DOB = ${years} years, ${month} months, and ${day} days`);
+
+
+// console.log(1970+55);
--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,3 +1,119 @@
 // push, pop, unshift, shift, join, includes, indexof,
 // join=> adds array to a string 
 // slice(just make a subarray) amd splice(cuts the arrayu original data )
+// const marvelheroes.push(dchero)
+// concat [...var,...] flat
+// nonprimitive datatype 
+// isArray , .from,.of
+// read from here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+let arr  = [12,"harry","sonu","monu",true, "ramu"]; 
+let str = "hello";//in js array stores heterogenous data 
+// ['h',e',l','l'.o']
+// console.log(arr);
+// console.log(arr[5]);
+// console.log(arr["harry"]); // not applicable
+// console.log(str[1]);
+// console.log("negative index in arr",arr.at(-1)); //negative indexing can be used using at()
+// push 
+const scores = [28,15,26,96,24,32];
+// console.log(scores.push(28));
+// console.log(scores.push(58));
+// console.log(scores);
+
+// pop
+scores.pop();
+// console.log(scores);
+
+// unshift
+scores.unshift(100); //Inserts new elements at the start of an array, and returns the new length of the array.
+// console.log(scores);
+
+scores.shift(); //Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+scores.shift();
+// console.log(scores);
+
+// join
+let arr1 = [10,20,30,40];
+// console.log(arr1);
+// console.log(arr1.join("separator -,#")); //A string used to separate one element of the array from the next in the resulting string. If omitted, the array elements are separated with a comma.
+
+// includes
+// console.log(arr1.includes(60)); //Determines whether an array includes a certain element, returning true or false as appropriate.
+
+//indexof
+// console.log(arr1.indexOf(30));
+
+// slice and splice 
+const stuData =["harry","abdul","priya","rahul","sanoj"];
+// console.log("A : ",stuData);
+
+// slice
+// console.log(stuData.slice(1,3));
+// console.log("after slice : ",stuData);
+
+// splice
+// console.log(stuData.splice(1,3)); // first : last index included and second it cuts the original one 
+// console.log("after splice : ",stuData);
+
+// +++++++++++++++++++++++++++++++++++++++ part2 ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const arr4 = [10,20,30];
+// console.log(arr4);
+// const arr3 = new Array(10,20,30);
+// console.log(arr3.length);
+
+const marvelHeroes = ["iron man ","captain america","hulk","thor"];
+const dcHeroes = ["superman","batman","flash","robin"];
+// console.log(marvelHeroes);
+// console.log(dcHeroes)
+
+// marvelHeroes.push(dcHeroes);
+// console.log(marvelHeroes);
+
+// console.log(marvelHeroes.concat(dcHeroes)) // pehle todega fir jodega 
+
+// separators
+let newHeroes = [...marvelHeroes,...dcHeroes]; // use triple dots to break down the array
+// console.log(newHeroes);
+
+// 2d array
+// let  matrix = [[10,20,30], 
+// // [40,50,60],
+// // [70,80,90]
+// // ]
+// // console.log(matrix);
+// // console.log(matrix[2][2]);
+
+const dynamicArray = [10,20,30,[40,50,60],[
+    [
+        [500,600,700]  
+    ]
+    ]
+    ];
+// console.log(dynamicArray)
+// console.log(dynamicArray[1])
+// console.log(dynamicArray[3])
+// console.log(dynamicArray[3][2])
+// console.log(dynamicArray[4][0][0][1])
+
+// flat 
+let openArray = dynamicArray.flat(Infinity)
+// console.log(openArray[7]);
+
+// Array.isArray
+console.log(Array.isArray(openArray)); //tell about the argument is array or not 
+
+// Array.from
+console.log(Array.from("hyyyyyyyyyy"));
+
+// Array.of
+console.log(Array.of("hyyyyyyyyy"))
+
+
+
+
+
+
+
+
+
