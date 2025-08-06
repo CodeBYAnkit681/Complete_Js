e10cb49701895db820656f034a0f0ba4a5fa14e4
Ankit Singh <as3542429@gmail.com>
2025-08-06 11:58:49 +0000

    ready for upcoming lec

---
+++ 04_controlFlows/01_ifelse.js
@@ -0,0 +1,2 @@
+// if, comparison operators , logical operators , if else, if else if laddder and if nesting
+//  som case of and or operator with if 
---
+++ 04_controlFlows/02_switch.js
@@ -0,0 +1,13 @@
+// switch syntax and ntro
+// default , break not ,string case value
+//truthy and falsy
+// falsy values = false , 0,-0,bigInt 0n,"",null,undefined,NAN
+//truthy values = true,"0",'false'," ",[],{},function(){},
+// Object.keys(obj).length ===0 , arr.length==0
+// false == 0
+//false == ''
+//0 == ''
+//nullish coaleshing operator
+// val = 5??10
+//ternary oparator
+//condition?true:false
