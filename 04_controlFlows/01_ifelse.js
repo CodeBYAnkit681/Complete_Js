e393e10440a696e5246782f2a1e5ed689c8bce42
Ankit Singh <as3542429@gmail.com>
2025-08-20 11:14:36 +0000

    flow controls started

--- 04_controlFlows/01_ifelse.js
+++ 04_controlFlows/01_ifelse.js
@@ -1,2 +1,17 @@
 // if, comparison operators , logical operators , if else, if else if laddder and if nesting
 //  som case of and or operator with if 
+
+// let arr = [1,2,5,3,6,4,5,65,6,55];
+// console.group(arr);
+let age = 25;
+if(age>18){
+    console.log("you are eligible  to vote")
+}
+
+//comparison operator 
+// >,<,==,===,>=,<=,!
+if(!true){
+    console.log("i am okay");
+}
+
+//logical operators
